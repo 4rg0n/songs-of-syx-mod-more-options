eed8c36dabfcfda07e4bf82b81704841cd9e5fd4
4rg0n <argonthechecker@gmail.com>
2026-06-29 18:13:34 +0200

    add doc blocks

--- mod-sdk/src/main/java/com/github/argon/sos/mod/sdk/ui/Button.java
+++ mod-sdk/src/main/java/com/github/argon/sos/mod/sdk/ui/Button.java
@@ -11,34 +11,58 @@ import snake2d.util.sprite.SPRITE;
  */
 public class Button<Value> extends AbstractButton<Value> {
 
+    /**
+     * @see AbstractButton#AbstractButton(Object)
+     */
     public Button(Value value) {
         super(value);
     }
 
+    /**
+     * @see AbstractButton#AbstractButton(CharSequence)
+     */
     public Button(CharSequence label) {
         super(label);
     }
 
+    /**
+     * @see AbstractButton#AbstractButton(CharSequence, CharSequence)
+     */
     public Button(CharSequence label, CharSequence description) {
         super(label, description);
     }
 
+    /**
+     * @see AbstractButton#AbstractButton(CharSequence, COLOR, CharSequence)
+     */
     public Button(CharSequence label, COLOR color, @Nullable CharSequence description) {
         super(label, color, description);
     }
 
+    /**
+     * @see AbstractButton#AbstractButton(SPRITE)
+     */
     public Button(SPRITE label) {
         super(label);
     }
 
+    /**
+     * @see AbstractButton#AbstractButton(SPRITE, COLOR)
+     */
     public Button(SPRITE label, COLOR color) {
         super(label, color);
     }
 
+    /**
+     * @see AbstractButton#AbstractButton(SPRITE, CharSequence)
+     */
     public Button(SPRITE label, @Nullable CharSequence description) {
         super(label, description);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Button<Value> searchTerm(@Nullable Value searchTerm) {
         super.searchTerm(searchTerm);
