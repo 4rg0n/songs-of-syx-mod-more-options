60f26512bd1ea64c230bc0fb1df2297fc27ad9aa
4rg0n <argonthechecker@gmail.com>
2026-06-30 15:17:15 +0200

    add doc blocks

--- mod-sdk/src/main/java/com/github/argon/sos/mod/sdk/game/error/DumpLogsException.java
+++ mod-sdk/src/main/java/com/github/argon/sos/mod/sdk/game/error/DumpLogsException.java
@@ -4,6 +4,9 @@ package com.github.argon.sos.mod.sdk.game.error;
  * Used to force the game to a log output into the error logs
  */
 public class DumpLogsException extends Throwable {
+    /**
+     * @see Throwable#Throwable(String)
+     */
     public DumpLogsException(String message) {
         super(message);
     }
